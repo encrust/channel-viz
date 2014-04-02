3a026cb9a1c704f34916947013d596bfbebb98ee
encrust <monty.barlow@ntlworld.com>
2014-04-02 09:07:16 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,11 +13,11 @@
 	*/
 
 	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+		defaultFeeds	= [6Lch4j2xl1gosDGzepPw4byS1U5HrVWjkCdAI8jQSWKcco4A], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Graphing thingy', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		dataColor		= 'a02020', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
